80077eab32422ae59f702103f73c1cf84c2b1f61
Aakash Gupta <97354708+AakashGupta24@users.noreply.github.com>
2023-09-24 23:54:53 +0530

    done

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2596be",
+        secondary: "#00008B",
+        dimWhite: "rgba(255, 235, 205, 0.7)",
+        dimBlue: "#2596be",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
